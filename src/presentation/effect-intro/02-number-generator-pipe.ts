8ed2cb6b37e3cc4c55772d1913fd366c4493e74d
Sebastian Lorenz <fubhy@fubhy.com>
2023-12-01 08:15:24 +0100

    chore: remove type sigs

--- src/presentation/effect-intro/02-number-generator-pipe.ts
+++ src/presentation/effect-intro/02-number-generator-pipe.ts
@@ -5,7 +5,7 @@ class UnluckyNumberError extends Data.TaggedError("UnluckyNumberError")<{}> {}
 class NumberTooLowError extends Data.TaggedError("NumberTooLowError")<{}> {}
 
 // A simple effect program using pipe syntax. Note how the type signature includes both possible error types.
-const fallible: Effect.Effect<never, UnluckyNumberError | NumberTooLowError, number> = Random.next.pipe(Effect.andThen((random) => {
+const fallible = Random.next.pipe(Effect.andThen((random) => {
   if (random === 13) {
     return Effect.fail(new UnluckyNumberError())
   }
@@ -18,7 +18,7 @@ const fallible: Effect.Effect<never, UnluckyNumberError | NumberTooLowError, num
 }))
 
 // Recovering from an error. Note how the error types have been eliminated from the type signature.
-const program: Effect.Effect<never, never, number> = fallible.pipe(Effect.catchTags({
+const program = fallible.pipe(Effect.catchTags({
   // We can recover from the "NumberTooLowError" by returning the lowest possible number.
   NumberTooLowError: (_) => Effect.succeed(5),
   // We can recover from the "UnluckyNumberError" error by returning a "lucky" number instead.
@@ -26,7 +26,7 @@ const program: Effect.Effect<never, never, number> = fallible.pipe(Effect.catchT
 }))
 
 // Unlike Promises, which are eagerly executed, Effects are descriptions of programs, they are not executed until we run them.
-const result: number = Effect.runSync(program)
+const result = Effect.runSync(program)
 
 // The result is a number, which is the result of the program.
 console.log(result)
--- src/presentation/effect-intro/03-number-generator-retry.ts
+++ src/presentation/effect-intro/03-number-generator-retry.ts
@@ -15,8 +15,8 @@ class UnluckyNumberError extends Data.TaggedError("UnluckyNumberError")<{}> {}
 class NumberTooLowError extends Data.TaggedError("NumberTooLowError")<{}> {}
 
 // A simple effect program using generator syntax. Note how the type signature includes both possible error types.
-const fallible: Effect.Effect<never, UnluckyNumberError | NumberTooLowError, number> = Effect.gen(function*($) {
-  const random: number = yield* $(Random.next)
+const fallible = Effect.gen(function*($) {
+  const random = yield* $(Random.next)
   if (random === 13) {
     return yield* $(new UnluckyNumberError())
   }
@@ -29,7 +29,7 @@ const fallible: Effect.Effect<never, UnluckyNumberError | NumberTooLowError, num
 })
 
 // Recovering from an error. Note how the error types have been eliminated from the type signature.
-const program: Effect.Effect<never, never, number> = fallible.pipe(Effect.catchTags({
+const program = fallible.pipe(Effect.catchTags({
   // We can recover from the "NumberTooLowError" by returning the lowest possible number.
   NumberTooLowError: (_) => Effect.retry(RetryPolicy),
   // We can recover from the "UnluckyNumberError" error by returning a "lucky" number instead.
@@ -37,7 +37,7 @@ const program: Effect.Effect<never, never, number> = fallible.pipe(Effect.catchT
 }))
 
 // Unlike Promises, which are eagerly executed, Effects are descriptions of programs, they are not executed until we run them.
-const result: number = Effect.runSync(program)
+const result = Effect.runSync(program)
 
 // The result is a number, which is the result of the program.
 console.log(result)
