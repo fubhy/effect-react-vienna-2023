f21a984666ccfecbf446433782fb59164e21bdd6
Sebastian Lorenz <fubhy@fubhy.com>
2023-12-05 12:46:52 +0100

    fix: use custom boundaries for timer

--- src/program/Metrics.ts
+++ src/program/Metrics.ts
@@ -1,4 +1,4 @@
-import { Metric } from "effect"
+import { Chunk, Metric } from "effect"
 
 export const SuccessCounter = Metric.counter("hackernews_fetch_success_count", {
   description: "The number of documents fetched successfully"
@@ -12,7 +12,37 @@ export const DefectCounter = Metric.counter("hackernews_fetch_defect_count", {
   description: "The number of defects encountered while fetching documents"
 })
 
-export const FetchTimer = Metric.timer("hackernews_fetch_timer")
+export const FetchTimer = Metric.timerWithBoundaries(
+  "hackernews_fetch_timer",
+  Chunk.fromIterable([
+    20,
+    40,
+    60,
+    80,
+    100,
+    110,
+    120,
+    130,
+    140,
+    150,
+    160,
+    170,
+    180,
+    190,
+    200,
+    250,
+    300,
+    350,
+    400,
+    450,
+    500,
+    600,
+    700,
+    800,
+    900,
+    1000
+  ])
+)
 
 export const BytesRead = Metric.counter("hackernews_bytes_read", {
   description: "The number of bytes read from HackerNews"
